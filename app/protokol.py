faeab8b94f2bf98e9119de2986135fe51036906f
bok11 <bok.will@gmail.com>
2017-11-10 15:15:04 +0100

    Calculate bits working

--- app/protokol.py
+++ app/protokol.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 
 class Protokol(object):
+    
+    def calcBits(self,n_nodes,n_sensors):
+        
+        if n_nodes is 1:
+            base = 52
+            result = base+n_sensors
+            
+        
+        else:
+            base = 52
+            result = base+n_sensors
+            
+            for nodes in range(n_nodes-1):
+                base = 8
+                result += base+n_sensors
+            
+        return result

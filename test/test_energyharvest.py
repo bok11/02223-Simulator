f21695e015c9a5febbdc97e9024f2ce4f77398ce
christianbillp <christianbillp@gmail.com>
2017-11-10 17:12:39 +0100

    Added energy test

--- test/test_energyharvest.py
+++ test/test_energyharvest.py
@@ -4,9 +4,16 @@ import unittest
 from app.energyharvest import EnergyHarvest 
 
 class testEnergyHarvest(unittest.TestCase):
-    
-        def setUp(self):
-            self. = Calculator()
-            
-            
-   
+
+    def setUp(self):
+        self.panel = EnergyHarvest()
+
+    def test_panel_charge(self):
+        result = self.panel.charge(panel_size=0.3, efficiency=0.2, month=3)
+        expected = 0.007
+        variance = 0.001
+        self.assertTrue(result >= expected - variance and result <= expected + variance)
+        #self.assertEqual(expected, result)
+
+if __name__ == '__main__':
+    unittest.main()
